540fa195257a450eb445e2909234fee780962784
Nur Kamilah Endra Savitri <kamilahendraes@gmail.com>
2026-09-20 18:18:42 +0700

    Labwork 01 - Prelab

---
+++ src/lw01/prelab/ColourPrint.java
@@ -0,0 +1,25 @@
+package lw01.prelab;
+
+public class ColourPrint extends PrintJob {
+
+    public ColourPrint(String id, int pages) {
+        super(id, pages);
+    }
+
+    @Override
+    public int calculateCharge() {
+        int pages = getPages();
+
+        if (pages <= 10) {
+            return (pages * 1500) + 2000;
+        } else {
+            return (10 * 1500) + ((pages - 10) * 1000) + 2000;
+        }
+    }
+
+    @Override
+    public String label() {
+        return "Colour";
+    }
+
+}
